a06bab77844108d40acd77a8d10a484d9933d11c
sonia francesconi <soniafra.93@gmail.com>
2024-05-24 08:21:50 +0100

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "cleaning database"
+Restaurant.destroy_all if Rails.env.development?
+
+puts "creating restaurants"
+
+Restaurant.create!(
+  name: "C'est Bon C'est Belge",
+  address: "Rue du Bon-Secours, Brussel",
+  phone_number: "+321 23 45 67 89",
+  category: "belgian"
+)
+
+Restaurant.create!(
+  name: "Hide",
+  address: "W13 7YU",
+  category: "japanese",
+  phone_number: "+331 23 45 67 89"
+)
+
+Restaurant.create!(
+  name: "Akoko",
+  address: "SE7 9OL",
+  category: "belgian",
+  phone_number: "+491 23 45 67 89"
+)
+
+Restaurant.create!(
+  name: "Mambow",
+  address: "SW6 0IL",
+  category: "chinese",
+  phone_number: "+311 23 45 67 89"
+)
+
+Restaurant.create!(
+  name: "Oita",
+  address: "E67 0PL",
+  category: "italian",
+  phone_number: "+441 23 45 67 89"
+)
+
+puts "Finished!"
